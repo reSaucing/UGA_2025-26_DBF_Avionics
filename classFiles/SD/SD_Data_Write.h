123cac768616ce9b3af100c57913b2bf98a4831f
Elvin Alvarez <elvin.sauc@gmail.com>
2025-10-19 17:39:37 -0400

    retrieve sensor data finished. thank gemini for all that code.

--- classFiles/SD/SD_Data_Write.h
+++ classFiles/SD/SD_Data_Write.h
@@ -1,10 +1,8 @@
 #ifndef SD_Data_Write_h
 #define SD_Data_Write_h
 
-#include <stdint.h>
 #include <SD.h>
 
-struct testData{int16_t test;};
 
 class SD_Data_Write{
 
@@ -12,10 +10,6 @@ class SD_Data_Write{
     SD_Data_Write();
 
     bool initSD();
-    bool createFlightlog();
-    bool writetoSD(testData &data);
   private:
-  char fileName[16];
-  File myFile;
 };
 #endif
