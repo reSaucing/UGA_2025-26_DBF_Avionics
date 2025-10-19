1a7acf0ec92f6d775884c3810ff77de54b345e64
Elvin Alvarez <elvin.sauc@gmail.com>
2025-10-19 18:20:57 -0400

    minor updates

--- classFiles/SD/SD_Data_Read.cpp
+++ classFiles/SD/SD_Data_Read.cpp
@@ -3,67 +3,52 @@
 #include <SD.h>
 
 SD_Data_Read::SD_Data_Read() {
-  // class constructor
+// class constructor
 }
 
 bool SD_Data_Read::initSD() {
-  pinMode(10, OUTPUT);
-  delay(5);
-  if (!SD.begin(10)) return false;
+  pinMode(10, OUTPUT);  //sets pin 10 as the output pin to identify the sd card via spi
+  delay(5); //5ms delay
+  if (!SD.begin(10)) return false;  //returns false if the sd card cannot be initialized
   return true;
 }
 
-// Lists all files in the root directory of the SD card.
 void SD_Data_Read::listFiles() {
+// Lists all files in the root directory of the SD card.
   Serial.println("\nFiles found on SD card:");
-  File root = SD.open("/");
+  File root = SD.open("/"); //open root directory
   while (true) {
-    File entry = root.openNextFile();
-    if (!entry) {
-      break; // no more files
-    }
-    if (!entry.isDirectory()) {
-      Serial.print(" - ");
+    File entry = root.openNextFile(); //sets entry equal to the name of the next file
+    if (!entry) break; // no more files. exits loop if entry does not exist
+    if (!entry.isDirectory()) { //lists directory name if it exists
+      Serial.print(" - "); 
       Serial.println(entry.name());
     }
-    entry.close();
+    entry.close();  //closes file
   }
-  root.close();
+  root.close(); //closes root directory
 }
-
-// Opens a specific log file for reading.
-bool SD_Data_Read::openLogFile(const char* filename) {
-  _myFile = SD.open(filename, FILE_READ);
-  return _myFile; // Returns true if the file was opened successfully
-}
-
-// Reads the next binary MasterLogEntry struct from the opened file.
-bool SD_Data_Read::readNextEntry(MasterLogEntry &data) {
-  if (!_myFile) {
-    return false; // No file is open
-  }
-  // read() returns the number of bytes read. If it matches the size of our struct,
-  // it means we successfully read one full entry.
-  return (_myFile.read((uint8_t *)&data, sizeof(data)) == sizeof(data));
-}
-
-// Closes the currently open file.
 void SD_Data_Read::closeLogFile() {
-  if (_myFile) {
-    _myFile.close();
-  }
+// Closes the currently open file.
+  if (myFile) myFile.close();
 }
 
-// Deletes a specific file from the SD card.
+bool SD_Data_Read::openLogFile(const char* filename) {
+// Opens a specific log file for reading.
+  myFile = SD.open(filename, FILE_READ); //opens the file with read privaleges
+  return myFile; // Returns true if the file was opened successfully
+}
+bool SD_Data_Read::readNextEntry(MasterLogEntry &data) {
+// Reads the next binary MasterLogEntry struct from the opened file.
+  if (!myFile) return false; // No file is open
+  // read() returns the number of bytes read. If it matches the size of our struct,
+  return (myFile.read((uint8_t *)&data, sizeof(data)) == sizeof(data));  //returns the number of bytes read. If it matches the size of our struct, returns true
+}
 bool SD_Data_Read::deleteFile(const char* filename) {
+// Deletes file from the SD card.
   Serial.print("Deleting file: ");
   Serial.print(filename);
   
-  if (SD.remove(filename)) {
-    Serial.println(" -> SUCCESS");
-    return true;
-  } else {
-    Serial.println(" -> FAILED");
-    return false;
-  }
+  if (SD.remove(filename)) return true;
+  else return false;
 }
--- dataLogTesting/retrieveSensorData/SD_Data_Read.h
+++ dataLogTesting/retrieveSensorData/SD_Data_Read.h
@@ -1,27 +1,24 @@
 #ifndef SD_Data_Read_h
 #define SD_Data_Read_h
 
+//headers added to pull structs 
 #include <SD.h>
-// We must include the headers for your data classes to define the MasterLogEntry struct
 #include "BNO_Data_Collect.h"
 #include "BMP_Data_Collect.h"
 #include "ASPD_Data_Collect.h"
 
-// This is the master data structure that the datalogger writes to the file.
-// It must match exactly.
 struct MasterLogEntry {
-  uint32_t timestamp_ms;
-  qVectors imu_data;
-  BaroPressure baro_data;
-  airSpeed airspeed_data;
+//master log has to match the format used in data write. this is used to parse all the bytes
+  uint32_t timestamp_ms;  //milliseconds
+  qVectors imu_data;  //imu vectors
+  BaroPressure baro_data; //pressure sensor data
+  airSpeed airspeed_data; //airspeed data
 };
 
 class SD_Data_Read {
  public:
-  // Constructor
   SD_Data_Read();
 
-  // Public Methods
   bool initSD();
   void listFiles();
   bool openLogFile(const char* filename);
@@ -30,8 +27,7 @@ class SD_Data_Read {
   bool deleteFile(const char* filename);
 
  private:
-  // The File object used for reading
-  File _myFile;
+  File myFile;
 };
 
 #endif
